791fa6e314a0e2b06b3d3810d11b2392a4c593a9
Marcin Sitko <m.sitko7@gmail.com>
2019-02-27 23:04:19 +0100

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TaskList';
+  newTask: string;
+  taskList: Array<string> = [];
+  doneList: Array<string> = [];
+
+  add() {
+    this.taskList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.taskList);
+  }
+
+  remove(task: string) {
+    this.taskList = this.taskList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.doneList.push(task);
+    this.remove(task);
+  }
 }
